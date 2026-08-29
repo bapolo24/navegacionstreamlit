d0f4878570e96a9fad14e2401e07f7f3246ee2eb
bapolo24 <byronjoseapoloaguilar@gmail.com>
2026-08-28 23:27:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st  # Importa la librería Streamlit con el alias st.
-inicio = st.Page("inicio.py", title="Inicio", icon="🏠", default=True)  # Define la página donde se ingresará el dato.
-api = st.Page("api.py", title="Resultado API", icon="🛢️")  # Define la página donde se utilizará el dato guardado.
-pagina = st.navigation([inicio, api])  # Registra las páginas de la aplicación.
+inicio = st.Page("inicio.py", title="Inicio", icon="🏠", default=True)  # Define la página principal.
+datos = st.Page("datos.py", title="Datos", icon="📊")  # Define la página que demostrará el uso de caché.
+pagina = st.navigation([inicio, datos])  # Registra las páginas de la aplicación multipágina.
 pagina.run()  # Ejecuta la página seleccionada por el usuario.
