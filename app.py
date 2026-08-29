b28539f82feafe05c533eb2a033e60f9829663f2
bapolo24 <byronjoseapoloaguilar@gmail.com>
2026-08-28 23:25:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st  # Importa la librería Streamlit con el alias st.
-inicio = st.Page("inicio.py", title="Inicio", icon="🏠", default=True)  # Define la página principal.
-api = st.Page("api.py", title="Grado API", icon="🛢️")  # Define la página de la calculadora.
-pagina = st.navigation([inicio, api])  # Registra las páginas disponibles dentro de la app.
-pagina.run()  # Ejecuta la página seleccionada en la navegación.
+inicio = st.Page("inicio.py", title="Inicio", icon="🏠", default=True)  # Define la página donde se ingresará el dato.
+api = st.Page("api.py", title="Resultado API", icon="🛢️")  # Define la página donde se utilizará el dato guardado.
+pagina = st.navigation([inicio, api])  # Registra las páginas de la aplicación.
+pagina.run()  # Ejecuta la página seleccionada por el usuario.
